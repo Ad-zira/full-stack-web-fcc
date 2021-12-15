4ed5543b618cfa5519a05dcfe9bf0706980437ea
Adamazira <aladdintirta@gmail.com>
2021-12-15 13:42:30 +0000

    Apply css styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,19 +3,52 @@
   const title = 'Todooo'
 </script>
 
+<style>
+  .todos {
+    width: 100%;
+    max-width: 42rem;
+    margin: 4rem auto 0 auto;
+  }
+
+  .new {
+    margin: 0 0 .5rem 0;
+  }
+
+  .new input {
+    font-size: 28px;
+    width: 100%;
+    padding: .5em 1em .3em 1em;
+    box-sizing: border-box;
+    background: rgba(255, 255, 255, .05);
+    border-radius: 8px;
+    text-align: center;
+  }
+
+  .todos :global(input) {
+    border: 1px solid transparent;
+  }
+
+  .todos :global(input:focus-visible) {
+    box-shadow: inset 1px 1px 6px rgba(0, 0, 0, 0.1);
+    border: 1px solid #ff3e00 !important;
+    outline: none;
+  } 
+</style>
+
 <svelte:head>
   <title>{title}</title>
 </svelte:head>
 
-<h1>{title}</h1>
-<!-- <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
-<form action="" method="">
-  <input type="text" aria-label="Add a todo" placeholder="+ type to add a todo" />
-
-</form>
-
-<TodoItem />
-<TodoItem />
-<TodoItem />
+<div class='todos'>
+  <h1>{title}</h1>
+  <form action="" method="" class="new">
+    <input type="text" aria-label="Add a todo" placeholder="+ type to add a todo" />
+    
+  </form>
+  
+  <TodoItem />
+  <TodoItem />
+  <TodoItem />  
+</div>
 
 
