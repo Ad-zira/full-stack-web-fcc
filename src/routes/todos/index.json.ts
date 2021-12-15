98b6cc15aa16cf33e6d293822deaa358c276a8df
Adamazira <aladdintirta@gmail.com>
2021-12-15 19:15:24 +0000

    Add GET and POST endpoints

---
+++ src/routes/todos/index.json.ts
@@ -0,0 +1,27 @@
+import type { RequestHandler } from "@sveltejs/kit"
+
+let todos: Todo[] = [];
+
+export const get: RequestHandler = () => {
+  return {
+    status: 200,
+    body: todos
+  }
+}
+
+export const post: RequestHandler<{}, FormData> = (req) => {
+  todos.push({
+    created_at: new Date(),
+    text: req.body.get('text'),
+    done: false
+  });
+  console.log(req.body.get("text"));
+
+  return {
+    status: 303,
+    headers: {
+      location: '/'
+    }
+  }
+}
+
